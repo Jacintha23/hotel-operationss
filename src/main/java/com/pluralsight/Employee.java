6b189c003ada127abba38b7d90884b7ae82274f3
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-05-06 11:27:49 -0700

    Added punchIn and punchOut methods

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -10,7 +10,8 @@ package com.pluralsight;
         private double regularHours;
         private double overTimeHours;
         private double totalPay;
-
+        private int punchInTime;
+        private int punchOutTime;
     public Employee(int employeeId, String name, String department, double payRate, double hoursWorked, double regularHours, double overTimeHours, double totalPay)
         {
             this.employeeId = employeeId;
@@ -86,4 +87,41 @@ package com.pluralsight;
     public void setTotalPay(double totalPay) {
         this.totalPay = totalPay;
     }
+
+    public void punchIn(int hour) //<---- set a variable
+    {
+        //There are less than two conditions/choices, use if statement over switch
+        //Each time the employee punches in, we will track their start time
+        //Each function will take an int as an input argument
+        //Employees will always clock in at the top of the hour. In 24-hour time.
+        if(hour < 1 || hour > 24)
+        { //This is an exception for if employees hours are over 24 hour
+            // use the throw statement to manually signal that something unexpected has happened
+            throw new RuntimeException("You must have hours between 1 and 24");
+        }
+
+
+
+    }
+
+    public void punchOut(int hour)
+    {
+        int startTime = hour;
+
+        if(startTime == 0)
+        {
+            throw new RuntimeException("You can't clock out unless you have clocked in");
+        }
+        if(hour < 1 || hour > 24)
+        {
+            throw new RuntimeException("You must have hours between 1 and 24");
+        }
+
+
+    }
+
+
+
+
+
 }
