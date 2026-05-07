3ba61f36a5568a3543b2146b7b04310125aebc87
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-05-06 21:18:46 -0700

    Created backing variables and constructors.

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
-public class Hotel {
+public class Hotel
+{
+    /*
+        * class will allow you to create a simple hotel object and check if there are rooms
+            available for the current day only.
+        * has 2 types of rooms
+             * a King Suite and
+             * a Basic Double room. (cannot combine)
+
+        * should track:
+             * the name,
+             * numberOfSuites,
+             * numberOfRooms,
+             * bookedSuites and
+             * bookedBasicRooms
+       * no public setters for these variables
+    */
+    // Backing Variables
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+    // Constructors
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms)
+    {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms)
+    {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
